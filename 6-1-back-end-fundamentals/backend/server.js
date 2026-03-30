d5c33da6f95b692c2d09185179041d6296b193e1
Naba-Alali <S202268320@kfupm.edu.sa>
2026-03-30 11:30:33 +0300

    todo3

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 
 
 
 // TODO 2: Create the Express app and store it in a variable named app
+const app = express();
+
 
 
 
 // TODO 3: Allow React to access the server
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 
 
